19e985e5f5e13697b45de9e73f05abeb9358e529
Benaiah Yusuf <yusufbenaiah@gmail.com>
2019-11-29 13:52:26 +0100

    fixed bcrypt bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const randomId = require('../helpers/identity.js');
 require('../models/db/user')();
 const db = require('../models/db/index');
